3d26ea36d816829f6800c9c60c8d6cfdd7b4d80d
mattlebo177 <mattlebo177@gmail.com>
2016-12-20 21:26:04 -0500

    Add files via upload

---
+++ photos/anthracitemines/anthracitemines.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+
+<!-- 2/28/2016 -->
+
+<html>
+
+	<head>
+		<title>Anthracite Mines</title>
+	
+		<meta name="description" content="Anthracite Mines">
+		<meta name=viewport content="width=device-width, initial-scale=1"> <!-- Optimizes mobile device screen width -->
+	
+		<link rel="icon" type="image/x-icon" href="/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="/styles.css">
+	</head>
+	
+	<body>
+		<h1>Anthracite Mines</h1>
+		<br/<br/>
+
+		<h3>Reading RR Frackville Branch Looking North from St. Clair</h3>
+		<!-- image of mine -->
+		<figure>
+			<img src="frackvillebranch.jpg" title="Outside View" alt="Outside View">
+		</figure>
+		<br/><br/>
+
+		<h3>Repplier Mine Entrance in St. Clair (Broad Mountain Coal Veins)</h3>
+		<!-- image of mine -->
+		<figure>
+			<img src="repplier.jpg" title="Outside View" alt="Outside View">
+		</figure>
+		<br/><br/>
+
+		<h3>Two Entrances to the Tunnel Mine in Ashland (Broad Mountain Coal Veins)</h3>
+		<!-- image of mines -->
+		<figure>
+			<img src="tunnelmines.jpg" title="Two Entrances to the Tunnel Mine in Ashland" alt="Two Entrances to the Tunnel Mine in Ashland">
+		</figure>
+		<br/><br/>
+
+		<h3>Intact Headframe</h3>
+		<!-- image from mine -->
+		<figure>
+			<img src="mineheadframe.jpg" title="Intact Headframe" alt="Intact Headframe">
+		</figure>
+		<br/><br/>
+
+		<h3>Looking Inside the First Opening</h3>
+		<!-- image from mine -->
+		<figure>
+			<img src="headframeinside.jpg" title="First Opening" alt="First Opening">
+		</figure>
+		<br/><br/>
+
+		<h3>Second Opening</h3>
+		<!-- image from mine -->
+		<figure>
+			<img src="secondopening.jpg" title="Looking West from Inside Tower" alt="Looking West from Inside Tower">
+		</figure>
+		<br/><br/>
+
+		<h3>Inside Second Opening</h3>
+		<!-- image from mine -->
+		<figure>
+			<img src="insidesecondopening.jpg" title="Inside Second Opening" alt="Inside Second Opening">
+		</figure>
+		<br/><br/>
+		
+		<h3>Potts Mine drainage</h3>
+		<!-- image from potts mine -->
+		<figure>
+			<img src="pottsamd.jpg" title="Potts - Acid Mine Drainage" alt="Potts - Acid Mine Drainage">
+		</figure>
+		<br/><br/>
+
+		<h3>Collapsed Shaft of Potts Mine</h3>
+		<!-- image from potts mine -->
+		<figure>
+			<img src="collapsedshaft.jpg" title="Collapsed Shaft of Potts Mine" alt="Potts - Acid Mine Drainage">
+		</figure>
+		<br/><br/>
+		
+		<h3>Merrian Slope Hoist Motor</h3>
+		<!-- image from merrian -->
+		<figure>
+			<img src="DSC_7755.jpg" title="Merrian Slope Hoist Motor" alt="Merrian Slope Hoist Motor">
+		</figure>
+		<br/><br/>
+		
+		<h3>Merrian Slope Hoist Controls</h3>
+		<!-- image from merrian -->
+		<figure>
+			<img src="DSC_7763.jpg" title="Merrian Slope Hoist Controls" alt="Merrian Slope Hoist Controls">
+		</figure>
+		<br/><br/>
+		
+		<h3>Merrian Slope</h3>
+		<!-- image from merrian -->
+		<figure>
+			<img src="DSC_7765.jpg" title="Merrian Slope - 1919" alt="Merrian Slope - 1919">
+		</figure>
+		<br/><br/>
+		
+		<h3>Inside Merrian Slope</h3>
+		<!-- image from merrian -->
+		<figure>
+			<img src="DSC_7768.jpg" title="Inside Merrian Slope" alt="Inside Merrian Slope">
+		</figure>
+		<br/><br/>
+		
+		<h3>Merrian Slope Collapse</h3>
+		<!-- image from merrian -->
+		<figure>
+			<img src="DSC_7773.jpg" title="Merrian Slope Collapse" alt="Merrian Slope Collapse">
+		</figure>
+		<br/><br/>
+		
+		<h3>East Ridge Colliery Slope 1</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7949.jpg" title="East Ridge Colliery Slope 1" alt="East Ridge Colliery Slope 1">
+		</figure>
+		<br/><br/>
+		
+		<h3>Looking Down Slope</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7950.jpg" title="East Ridge Colliery Slope 1" alt="East Ridge Colliery Slope 1">
+		</figure>
+		<br/><br/>
+		
+		<h3>East Ridge Colliery Slope 2</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7952.jpg" title="East Ridge Colliery Slope 2" alt="East Ridge Colliery Slope 2">
+		</figure>
+		<br/><br/>
+		
+		<h3>Volperis Colliery #6 Slope</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7962.jpg" title="Volperis Colliery #6 Slope" alt="Volperis Colliery #6 Slope">
+		</figure>
+		<br/><br/>
+		
+		<h3>East Ridge Colliery Slope 3</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7968.jpg" title="East Ridge Colliery Slope 3" alt="East Ridge Colliery Slope 3">
+		</figure>
+		<br/><br/>
+		
+		<h3>Another Unknown Slope</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_7973.jpg" title="Slope" alt="Slope">
+		</figure>
+		<br/><br/>
+		
+		<h3>Foundation North of St. Clair</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8001.jpg" title="Foundation North of St. Clair" alt="Foundation North of St. Clair">
+		</figure>
+		<br/><br/>
+		
+		<h3>Eagle Hill Colliery - Main Buck Slope</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8005.jpg" title="Buck Slope" alt="Buck Slope">
+		</figure>
+		<br/><br/>
+		
+		<h3>Main Buck Slope Hoist Remains</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8006.jpg" title="Main Buck Slope Hoist Remains" alt="Main Buck Slope Hoist Remains">
+		</figure>
+		<br/><br/>
+		
+		<h3>Main Buck Slope 1910 Date Stone</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8007.jpg" title="Main Buck Slope Date Stone" alt="Main Buck Slope Date Stone">
+		</figure>
+		<br/><br/>
+		
+		<h3>Looking Down Main Buck Slope</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8008.jpg" title="Looking Down Main Buck Slope" alt="Looking Down Main Buck Slope">
+		</figure>
+		<br/><br/>
+		
+		<h3>Culm Pile Around Entrance</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8009.jpg" title="Culm Pile Around Entrance" alt="Culm Pile Around Entrance">
+		</figure>
+		<br/><br/>
+		
+		<h3>Buck Slope Air Shaft</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8018.jpg" title="Buck Slope Air Shaft" alt="Buck Slope Air Shaft">
+		</figure>
+		<br/><br/>
+		
+		<h3>Looking South into St. Clair from Broad Mountain</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8022.jpg" title="Looking South into St. Clair from Broad Mountain" alt="Looking South into St. Clair from Broad Mountain">
+		</figure>
+		<br/><br/>
+		
+		<h3>Former Location of Herbine Coal Conveyor</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8024.jpg" title="Former Location of Herbine Coal Conveyor" alt="Former Location of Herbine Coal Conveyor">
+		</figure>
+		<br/><br/>
+		
+		<h3>Conveyor Remains</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8030.jpg" title="Conveyor Remains" alt="Conveyor Remains">
+		</figure>
+		<br/><br/>
+		
+		<h3>Damp Airflow from Herbine Shaft</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8038.jpg" title="Herbine Shaft" alt="Herbine Shaft">
+		</figure>
+		<br/><br/>
+		
+		<h3>Herbine Shaft</h3>
+		<!-- image from st. clair -->
+		<figure>
+			<img src="DSC_8039.jpg" title="Herbine Shaft" alt="Herbine Shaft">
+		</figure>
+		<br/><br/>
+		
+		<h3>Oak Hill Colliery Shaft</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_8052.jpg" title="Oak Hill Colliery Shaft" alt="Oak Hill Colliery Shaft">
+		</figure>
+		<br/><br/>
+		
+		<h3>Heckshersville Freight Station - Reading RR</h3>
+		<!-- image from heckshersville -->
+		<figure>
+			<img src="DSC_8059.jpg" title="Heckshersville Freight Station - Reading RR" alt="Heckshersville Freight Station - Reading RR">
+		</figure>
+		<br/><br/>
+
+		<h3>Lincoln Colliery Air Shaft</h3>
+		<!-- image from lorberry -->
+		<figure>
+			<img src="DSC_8081.jpg" title="Licoln Colliery Air Shaft" alt="Licoln Colliery Air Shaft">
+		</figure>
+		<br/><br/>
+
+		<h3>Bootleg Hoist House</h3>
+		<!-- image from hecksherville -->
+		<figure>
+			<img src="DSC_8118.jpg" title="Bootleg Hoist House" alt="Bootleg Hoist House">
+		</figure>
+		<br/><br/>
+
+		<h3>Bootleg Drift Entrance</h3>
+		<!-- image from hecksherville -->
+		<figure>
+			<img src="DSC_8138.jpg" title="Bootleg Drift Entrance" alt="Bootleg Drift Entrance">
+		</figure>
+		<br/><br/>
+
+		<h3>Bootleg Air Fan Engine</h3>
+		<!-- image from hecksherville -->
+		<figure>
+			<img src="DSC_8142.jpg" title="Bootleg Air Fan Engine" alt="Bootleg Air Fan Engine">
+		</figure>
+		<br/><br/>
+
+		<h3>Bootleg Air Hole</h3>
+		<!-- image from hecksherville -->
+		<figure>
+			<img src="DSC_8143.jpg" title="Bootleg Air Hole" alt="Bootleg Air Hole">
+		</figure>
+		<br/><br/>
+
+		<h3>Bootleg Air Fan</h3>
+		<!-- image from hecksherville -->
+		<figure>
+			<img src="DSC_8145.jpg" title="Bootleg Air Fan" alt="Bootleg Air Fan">
+		</figure>
+		<br/><br/>
+
+			<h2>More Information</h2>
+			
+			<a href = "http://www.facstaff.bucknell.edu/marsh/anthracite_towns.pdf">Continuity and Decline in the Anthracite Towns of Pennsylvania, Bucknell Univ. [PDF]</a><br/>
+			<a href = "https://ehistory.osu.edu/exhibitions/gildedage/content/AnthraciteRhone">Anthracite Coal Mines and Mining, Ohio State Univ.</a><br/>
+			<a href = "http://www.minemaps.psu.edu/">PA Mine Map Atlas, PSU</a><br/>
+			<a href = "http://www.depgis.state.pa.us/emappa/">PA DEP GIS Mapping</a><br/>
+
+	</body>
+	<br/>
+	
+	<footer>
+	<!-- Navigation bar -->
+	<?php 
+		include ('../../navbar.php'); 
+		echo "<br/><br/>";
+		include ('../../sitemessage.php');
+	?>
+
+	</footer>
+
+</html>

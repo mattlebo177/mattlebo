9eb1d341758ee5e4afadc52e0c3a4f4155dca464
mattlebo177 <mattlebo177@gmail.com>
2019-10-25 17:43:44 -0400

    Add files via upload

--- employeedocs/index.php
+++ employeedocs/index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 
-<!-- 10/9/2015 -->
+<!-- 10/25/2019 -->
 
 <html>
 
 	<head>
+
+		<!-- Navigation Bar -->
+		<?php
+			include ('../navbar.php');
+		?>
+
 		<title>Employee Portal</title>
 	
 		<meta name="description" content="Employee Portal">
@@ -18,24 +24,25 @@
 		<h1>Employee Portal</h1>
 
 		<p>
-			<a href = "http://www.mattlebo.com:10000">Webmin (Raspberry Pi)</a><br/>
-			<a href = "http://www.mattlebo.com:10001">Webmin (PowerEdge R610)</a><br/>
-			<a href = "https://www.mattlebo.com">PowerEdge R610 iDRAC</a><br/>
+			<a href = "https://www.mattlebo.com/ui/#/login">ESXi</a><br/>
+			<a href = "https://www.mattlebo.com:444">iDRAC (PowerEdge R710)</a><br/>
+			<a href = "https://www.mattlebo.com:8443">Unifi Controller</a><br/>
 			<a href = "ftp://www.mattlebo.com">FTP Site</a><br/>
+			<a href = "/employeedocs/siteconfig">Server Config Files</a><br/>
 			<a href = "https://github.com/mattlebo177/mattlebo.com/">GitHub Website Source Code</a><br/>
-			<a href = "/employeedocs/network_map.png">Network Map</a><br/></p>
+			<a href = "https://docs.google.com/spreadsheets/d/1er4BAfbCUE7QL5Whh4M2pYk0pWwamfBLVNoltco1xWI/edit?usp=sharing">Network Plan</a><br/>
+			<a href = "http://www.mattlebo.com:82/harrisburg_division/">ATCS Monitor - NS HBG DIV</a><br/></p>
 
 	<br/><br/>
 	</body>
 	
 	<footer>
-	<!-- Navigation bar -->
-	<?php 
-		include ('../navbar.php');
-		echo "<br/><br/>";
+	
+	<!-- Site Message -->
+	<?php
 		include ('../sitemessage.php');
 	?>
-	
+
 	</footer>
 
 
